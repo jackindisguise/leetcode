4b66112a5fad885fc44b9ff8efeb7260764ed49b
nowotato <incognitus.me@gmail.com>
2021-10-20 18:20:00 -0500

    fix: Added comments.

--- 006.js
+++ 006.js
@@ -4,15 +4,19 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
+	// stupid cases
     if(s.length==1) return s;
     if(numRows==1) return s;
+
+	// generate the zigzag pattern
 	let seq = [];
     for(let j=0;j<s.length;j++){
-        let row = j%((numRows*2)-2)
-        if(row>=numRows) row=numRows-(row-numRows)-2;
-        if(seq[row]) seq[row] += s[j];
-        else seq[row]=s[j];
+        let row = j%((numRows*2)-2) // where in this zigzag sequence does this letter appear
+        if(row>=numRows) row=numRows-(row-numRows)-2; // we're in the zig part
+        if(seq[row]) seq[row] += s[j]; // add this letter to its respective row
+        else seq[row]=s[j]; // we haven't seen this row yet, generate it
     }
 
+	// join all rows into 1 line
 	return seq.join("");
 };
