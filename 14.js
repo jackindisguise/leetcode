7f49854d598f7f73de3cd6a78cd1ea4bfc55819f
nowotato <incognitus.me@gmail.com>
2022-08-26 22:39:51 -0500

    feat: Added solution to 14. Longest Common Prefix.

---
+++ 14.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length===1) return strs[0]; // stupid case
+	// the first element must match with every other element (starting from the beginning), so we don't need to do anything fancy.
+	// we can just compare the first element to every other element, wittling down the "matching length" by comparing each element from the back
+    let root = strs[0];
+    let match = root.length;
+    for(let i=1;i<strs.length;i++){
+        let str = strs[i];
+        match = Math.min(match, str.length); // only start comparison from the end of the known match length so far (unless this string is shorter)
+		// starting from the back, if any character doesn't match, that is the new end of the matching length
+        for(let j=match-1;j>=0;j--) if(str[j] != root[j]) match=j;
+        if(match===0) return "";
+    }
+
+    return root.substring(0,match);
+};
